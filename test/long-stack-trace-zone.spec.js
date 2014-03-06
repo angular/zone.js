01ce3b31236b6a10ff4e8eea48009dd1f339aba2
Brian Ford <btford@umich.edu>
2014-03-06 09:45:22 -0800

    fix: long stack trace test

--- test/long-stack-trace-zone.spec.js
+++ test/long-stack-trace-zone.spec.js
@@ -27,7 +27,6 @@ describe('Zone.patch', function () {
     jasmine.Clock.tick(0);
 
     expect(log[0]).toBe('Error: hello');
-    dump(log[1])
-    expect(log[1].split('--- ').length).toBe(3);
+    expect(log[1].split('--- ').length).toBe(4);
   });
 });
