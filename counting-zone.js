86328fb5b4723354c2832b50583dc3093e494980
Brian Ford <btford@umich.edu>
2014-05-07 03:31:22 -0700

    feat: improve countingZone and example

--- counting-zone.js
+++ counting-zone.js
@@ -5,18 +5,18 @@
 Zone.countingZone = {
 
   // setTimeout
-  enqueueTask: function () {
+  '+enqueueTask': function () {
     this.data.count += 1;
   },
 
   // fires when...
   // - clearTimeout
   // - setTimeout finishes
-  dequeueTask: function () {
+  '-dequeueTask': function () {
     this.data.count -= 1;
   },
 
-  afterTask: function () {
+  '+afterTask': function () {
     if (this.data.count === 0 && !this.data.flushed) {
       this.data.flushed = true;
       this.run(this.onFlush);
