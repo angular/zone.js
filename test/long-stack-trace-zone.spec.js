d9f6c56771c2b73bbacc29a118b7299c90b2748c
Matthew Hill <matthew.jh@outlook.com>
2015-09-08 12:51:26 +0100

    fix(longStackTraceZone): modifies stackFramesFilter to exclude zone.js frames

--- test/long-stack-trace-zone.spec.js
+++ test/long-stack-trace-zone.spec.js
@@ -19,6 +19,7 @@ describe('longStackTraceZone', function () {
         setTimeout(function () {
           setTimeout(function () {
             expect(log[0]).toBe('Error: hello');
+
             expect(log[1].split('--- ').length).toBe(4);
             done();
           }, 0);
@@ -28,6 +29,13 @@ describe('longStackTraceZone', function () {
     });
   });
 
+  it('should filter out zone.js frames with default stackFramesFilter impl', function () {
+    var zoneFrame = 'at Zone.bind (http://localhost:8080/node_modules/zone.js/dist/zone.js:84:48)';
+    var nonZoneFrame = 'at a (http://localhost:8080/index.js:7:3)';
+
+    expect(lstz.stackFramesFilter(zoneFrame)).toBe(false);
+    expect(lstz.stackFramesFilter(nonZoneFrame)).toBe(true);
+  });
 
   it('should filter based on stackFramesFilter', function (done) {
     lstz.fork({
