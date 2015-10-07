cc1e9738a79c13f08484d69e3f74c9ee5fe67aa6
Marc Laval <mlaval@amadeus.com>
2015-10-07 11:27:21 +0200

    chore(saucelabs): update browser list
    Closes #192

--- sauce.conf.js
+++ sauce.conf.js
@@ -5,64 +5,111 @@ module.exports = function (config) {
   config.files.unshift('test/saucelabs.js');
 
   var customLaunchers = {
-    'SL_Chrome': {
+    'SL_CHROME': {
       base: 'SauceLabs',
       browserName: 'chrome',
-      version: '42'
+      version: '48'
     },
-    /*'SL_ChromeBeta': {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      version: 'beta'
-    },*/
-    'SL_Firefox': {
+    'SL_FIREFOX': {
       base: 'SauceLabs',
       browserName: 'firefox',
-      version: '37'
+      version: '44'
     },
-    /*
-    'SL_Safari7': {
+    /*'SL_SAFARI7': {
       base: 'SauceLabs',
-      browserName: 'safari',
-      platform: 'OS X 10.9',
-      version: '7'
+        browserName: 'safari',
+        platform: 'OS X 10.9',
+        version: '7'
+    },*/
+    'SL_SAFARI8': {
+      base: 'SauceLabs',
+        browserName: 'safari',
+        platform: 'OS X 10.10',
+        version: '8'
     },
-    */
-    'SL_Safari8': {
+    'SL_SAFARI9': {
       base: 'SauceLabs',
-      browserName: 'safari',
+        browserName: 'safari',
+        platform: 'OS X 10.11',
+        version: '9.0'
+    },
+    /*'SL_IOS7': {
+      base: 'SauceLabs',
+      browserName: 'iphone',
       platform: 'OS X 10.10',
-      version: '8'
+      version: '7.1'
+    },*/
+    'SL_IOS8': {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.10',
+      version: '8.4'
     },
-    'SL_IE_9': {
+    /*'SL_IOS9': {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.10',
+      version: '9.2'
+    },*/
+    'SL_IE9': {
       base: 'SauceLabs',
       browserName: 'internet explorer',
       platform: 'Windows 2008',
       version: '9'
     },
-    'SL_IE_10': {
+    'SL_IE10': {
       base: 'SauceLabs',
       browserName: 'internet explorer',
       platform: 'Windows 2012',
       version: '10'
     },
-    'SL_IE_11': {
+    'SL_IE11': {
       base: 'SauceLabs',
       browserName: 'internet explorer',
-      platform: 'Windows 8.1',
+      platform: 'Windows 10',
       version: '11'
+    },
+    /*'SL_MSEDGE13': {
+      base: 'SauceLabs',
+      browserName: 'MicrosoftEdge',
+      platform: 'Windows 10',
+      version: '13.10586'
+    },*/
+    'SL_ANDROID4.1': {
+      base: 'SauceLabs',
+      browserName: 'android',
+      platform: 'Linux',
+      version: '4.1'
+    },
+    'SL_ANDROID4.2': {
+      base: 'SauceLabs',
+      browserName: 'android',
+      platform: 'Linux',
+      version: '4.2'
     },
     'SL_ANDROID4.3': {
       base: 'SauceLabs',
       browserName: 'android',
       platform: 'Linux',
       version: '4.3'
+    },
+    'SL_ANDROID4.4': {
+      base: 'SauceLabs',
+      browserName: 'android',
+      platform: 'Linux',
+      version: '4.4'
+    },
+    'SL_ANDROID5.1': {
+      base: 'SauceLabs',
+      browserName: 'android',
+      platform: 'Linux',
+      version: '5.1'
     }
   };
 
   config.set({
     captureTimeout: 120000,
-    browserNoActivityTimeout: 1500000,
+    browserNoActivityTimeout: 240000,
 
     sauceLabs: {
       testName: 'Zone.js',
@@ -70,7 +117,7 @@ module.exports = function (config) {
       recordVideo: false,
       recordScreenshots: false,
       options:  {
-          'selenium-version': '2.45.0',
+          'selenium-version': '2.53.0',
           'command-timeout': 600,
           'idle-timeout': 600,
           'max-duration': 5400
