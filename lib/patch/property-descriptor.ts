8400b3fa69d12bed02c36201a726bccbaba306e3
Jeff Cross <middlefloor@gmail.com>
2016-02-13 09:29:34 -0800

    Revert "fix(WebWorker): Patch WebSockets and XMLHttpRequest in WebWorker"
    This reverts commit 9041a3a5d8acf010b5a07dda577b8dd9920d4693.
    
    This commit causes issues in node because of accessing XMLHttpRequest.

--- lib/patch/property-descriptor.ts
+++ lib/patch/property-descriptor.ts
@@ -5,24 +5,25 @@ import * as keys from '../keys';
 var eventNames = 'copy cut paste abort blur focus canplay canplaythrough change click contextmenu dblclick drag dragend dragenter dragleave dragover dragstart drop durationchange emptied ended input invalid keydown keypress keyup load loadeddata loadedmetadata loadstart message mousedown mouseenter mouseleave mousemove mouseout mouseover mouseup pause play playing progress ratechange reset scroll seeked seeking select show stalled submit suspend timeupdate volumechange waiting mozfullscreenchange mozfullscreenerror mozpointerlockchange mozpointerlockerror error webglcontextrestored webglcontextlost webglcontextcreationerror'.split(' ');
 
 export function apply() {
+  if (utils.isWebWorker()){
+    // on WebWorker so don't apply patch
+    return;
+  }
+
   var supportsWebSocket = typeof WebSocket !== 'undefined';
   if (canPatchViaPropertyDescriptor()) {
     // for browsers that we can patch the descriptor:  Chrome & Firefox
-    if (!utils.isWebWorker()){
-      var onEventNames = eventNames.map(function (property) {
-        return 'on' + property;
-      });
-      utils.patchProperties(HTMLElement.prototype, onEventNames);
-    }
+    var onEventNames = eventNames.map(function (property) {
+      return 'on' + property;
+    });
+    utils.patchProperties(HTMLElement.prototype, onEventNames);
     utils.patchProperties(XMLHttpRequest.prototype);
     if (supportsWebSocket) {
       utils.patchProperties(WebSocket.prototype);
     }
   } else {
     // Safari, Android browsers (Jelly Bean)
-    if (!utils.isWebWorker()){
-      patchViaCapturingAllTheEvents();
-    }
+    patchViaCapturingAllTheEvents();
     utils.patchClass('XMLHttpRequest');
     if (supportsWebSocket) {
       webSocketPatch.apply();
@@ -31,22 +32,21 @@ export function apply() {
 }
 
 function canPatchViaPropertyDescriptor() {
-  if (!utils.isWebWorker() && !Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'onclick')
-      && typeof Element !== 'undefined') {
+  if (!Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'onclick') && typeof Element !== 'undefined') {
     // WebKit https://bugs.webkit.org/show_bug.cgi?id=134364
     // IDL interface attributes are not configurable
     var desc = Object.getOwnPropertyDescriptor(Element.prototype, 'onclick');
     if (desc && !desc.configurable) return false;
   }
 
-  Object.defineProperty(XMLHttpRequest.prototype, 'onreadystatechange', {
+  Object.defineProperty(HTMLElement.prototype, 'onclick', {
     get: function () {
       return true;
     }
   });
-  var req = new XMLHttpRequest();
-  var result = !!req.onreadystatechange;
-  Object.defineProperty(XMLHttpRequest.prototype, 'onreadystatechange', {});
+  var elt = document.createElement('div');
+  var result = !!elt.onclick;
+  Object.defineProperty(HTMLElement.prototype, 'onclick', {});
   return result;
 };
 
