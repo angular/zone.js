9a7e3c03c841b07450b678f21bdebb437e18a76e
Marc Laval <mlaval@amadeus.com>
2016-08-10 11:22:49 +0200

    fix(browser): make Object.defineProperty patch safer
    Fixes #391

--- lib/browser/define-property.ts
+++ lib/browser/define-property.ts
@@ -14,10 +14,11 @@ export function propertyPatch() {
     if (isUnconfigurable(obj, prop)) {
       throw new TypeError('Cannot assign to read only property \'' + prop + '\' of ' + obj);
     }
+    const originalConfigurableFlag = desc.configurable;
     if (prop !== 'prototype') {
       desc = rewriteDescriptor(obj, prop, desc);
     }
-    return _defineProperty(obj, prop, desc);
+    return _tryDefineProperty(obj, prop, desc, originalConfigurableFlag);
   };
 
   Object.defineProperties = function (obj, props) {
@@ -46,8 +47,9 @@ export function propertyPatch() {
 };
 
 export function _redefineProperty(obj, prop, desc) {
+  const originalConfigurableFlag = desc.configurable;
   desc = rewriteDescriptor(obj, prop, desc);
-  return _defineProperty(obj, prop, desc);
+  return _tryDefineProperty(obj, prop, desc, originalConfigurableFlag);
 };
 
 function isUnconfigurable (obj, prop) {
@@ -63,6 +65,25 @@ function rewriteDescriptor (obj, prop, desc) {
     obj[unconfigurablesKey][prop] = true;
   }
   return desc;
+}
+
+function _tryDefineProperty (obj, prop, desc, originalConfigurableFlag) {
+  try {
+    return _defineProperty(obj, prop, desc);
+  }
+  catch(e) {
+    if (desc.configurable) {
+      // In case of errors, when the configurable flag was likely set by rewriteDescriptor(), let's retry with the original flag value
+      if (typeof originalConfigurableFlag == 'undefined') {
+        delete desc.configurable;
+      } else {
+        desc.configurable = originalConfigurableFlag;
+      }
+      return _defineProperty(obj, prop, desc);
+    } else {
+      throw e;
+    }
+  }
 }
 
 
