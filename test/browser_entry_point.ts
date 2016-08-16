6ef745117ba49a89695c3b1b93d6f192fd00c38e
Misko Hevery <misko@hevery.com>
2016-08-16 16:59:43 -0700

    chore: clean up imports

--- test/browser_entry_point.ts
+++ test/browser_entry_point.ts
@@ -4,9 +4,13 @@ import './wtf_mock';
 // Setup tests for Zone without microtask support
 import '../lib/zone';
 import '../lib/browser/browser.ts';
+import '../lib/zone-spec/async-test';
+import '../lib/zone-spec/fake-async-test';
 import '../lib/zone-spec/long-stack-trace';
-import '../lib/zone-spec/wtf';
 import '../lib/zone-spec/proxy';
+import '../lib/zone-spec/sync-test';
+import '../lib/zone-spec/task-tracking';
+import '../lib/zone-spec/wtf';
 
 // Setup test environment
 import './test-env-setup';
--- test/node_entry_point.ts
+++ test/node_entry_point.ts
@@ -4,7 +4,12 @@ import './wtf_mock';
 // Setup tests for Zone without microtask support
 import '../lib/zone';
 import '../lib/node/node';
+import '../lib/zone-spec/async-test';
+import '../lib/zone-spec/fake-async-test';
 import '../lib/zone-spec/long-stack-trace';
+import '../lib/zone-spec/proxy';
+import '../lib/zone-spec/sync-test';
+import '../lib/zone-spec/task-tracking';
 import '../lib/zone-spec/wtf';
 
 // Setup test environment
