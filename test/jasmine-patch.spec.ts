ff88bb43d29711cd6d6c575f6855590455463ae3
Misko Hevery <misko@hevery.com>
2016-08-19 09:42:31 -0700

    fix(tasks): do not drain the microtask queue early.
    Fixes a bug where a event task invoked inside another task
    would drain the microtask queue too early. This would mean that microtasks
    would be called unexpectedly in the middle of what should have been
    a block of synchronous code.

--- test/jasmine-patch.spec.ts
+++ test/jasmine-patch.spec.ts
@@ -1,3 +1,8 @@
+beforeEach(() => {
+  // assert that each jasmine run has a task, so that drainMicrotask works properly.
+  expect(Zone.currentTask).toBeTruthy();
+});
+
 describe('jasmine', () => {
   let throwOnAsync = false;
   let beforeEachZone: Zone = null;
