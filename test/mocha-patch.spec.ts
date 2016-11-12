c99687f5c3376b77aae312ae4c592f76be27cc39
Kristoffer Nording <krinor@gmail.com>
2016-11-12 14:30:03 +0100

    Added tests for TDD-style with Mocha, to make sure everything is working as expected

--- test/mocha-patch.spec.ts
+++ test/mocha-patch.spec.ts
@@ -1,10 +1,26 @@
+// Extra Mocha-specific typings to make sure typescript compiler is happy
+// Didn't want to add @types/mocha because of duplication in typings-file with @types/jasmine
+declare function suite(description: string, suiteFn: () => void): void;
+declare function test(description: string, testFn: () => void): void;
+declare function specify(description: string, testFn: () => void): void;
+declare function setup(fn: () => void): void;
+declare function teardown(fn: () => void): void;
+declare function suiteSetup(fn: () => void): void;
+declare function suiteTeardown(fn: () => void): void;
+declare function before(fn: () => void): void;
+declare function after(fn: () => void): void;
+//
 
-
-describe('mocha', () => {
+describe('Mocha BDD-style', () => {
   let throwOnAsync = false;
   let beforeEachZone: Zone = null;
   let itZone: Zone = null;
   const syncZone = Zone.current;
+  let beforeZone: Zone = null;
+
+  before(() => {
+    beforeZone = Zone.current;
+  });
 
   try {
     Zone.current.scheduleMicroTask('dontallow', () => null);
@@ -27,4 +43,45 @@ describe('mocha', () => {
     expect(beforeEachZone).toBe(zone);
     expect(itZone).toBe(zone);
   });
+
+  after(() => {
+    expect(beforeZone).toBe(Zone.current);
+  });
+});
+
+suite('Mocha TDD-style', () => {
+  let testZone: Zone = null;
+  let beforeEachZone: Zone = null;
+  let suiteSetupZone: Zone = null;
+
+  suiteSetup(() => {
+    suiteSetupZone = Zone.current;
+  });
+
+  setup(() => {
+    beforeEachZone = Zone.current;
+  });
+
+  test('should run in Zone with "test"-syntax in TDD-mode', () => {
+    testZone = Zone.current;
+    expect(Zone.currentTask).toBeTruthy();
+    expect(testZone.name).toEqual('ProxyZone'); 
+  });
+
+  specify('test should run in Zone with "specify"-syntax in TDD-mode', () => {
+    testZone = Zone.current;
+    expect(Zone.currentTask).toBeTruthy();
+    expect(testZone.name).toEqual('ProxyZone'); 
+  });
+
+  teardown(() => {
+    expect(Zone.current.name).toEqual('ProxyZone');
+    expect(beforeEachZone).toBe(Zone.current);
+    expect(testZone).toBe(Zone.current);
+  });
+
+  suiteTeardown(() => {
+    expect(suiteSetupZone).toBe(Zone.current);
+  });
+
 });
