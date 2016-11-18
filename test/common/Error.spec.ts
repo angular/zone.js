e1c2a02c87ba7829915f98e7fabd61047f7f4db9
Misko Hevery <misko@hevery.com>
2016-11-18 11:38:55 -0800

    feat(Error): Rewrite Error stack frames to include zone
    The errors caused by the Zone contain extra stack frames. This feature
    removes the confusing stack frames as well as appends the zone for each
    stack frame to get a better understanding of the error messages.
    
    Improved Error:
    ```
    Error.spec.js:54 Error: Inside [InnerZone]
        at insideRun (Error.spec.js:31) [InnerZone]
        at Zone.run (zone.js:100) [<root> => InnerZone]
        at testFn (Error.spec.js:29) [<root>]
        at Zone.run (zone.js:100) [ProxyZone => <root>]
        at Object.eval (Error.spec.js:19) [ProxyZone]
    ```
    
    Original Error:
    ```
    Error.spec.js:53 Error: Inside
        at ZoneAwareError (zone.js:652)
        at insideRun (Error.spec.js:31)
        at ZoneDelegate.invoke (zone.js:216)
        at Zone.run (zone.js:100)
        at testFn (Error.spec.js:29)
        at ZoneDelegate.invoke (zone.js:216)
        at Zone.run (zone.js:100)
        at Object.eval (Error.spec.js:19)
    ```

---
+++ test/common/Error.spec.ts
@@ -0,0 +1,69 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+describe('ZoneAwareError', () => {
+  // If the environment does not supports stack rewrites, then these tests will fail
+  // and there is no point in running them.
+  if (!Error['stackRewrite']) return;
+
+  it('should show zone names in stack frames and remove extra frames', () => {
+    const rootZone = getRootZone();
+    const innerZone = rootZone.fork({name: 'InnerZone'});
+
+    rootZone.run(testFn);
+    function testFn() {
+      let outside: Error;
+      let inside: Error;
+      try {
+        throw new Error('Outside');
+      } catch(e) {
+        outside = e;
+      }
+      innerZone.run(function insideRun () {
+        try {
+          throw new Error('Inside');
+        } catch(e) {
+          inside = e;
+        }
+      });
+
+      expect(outside.stack).toEqual(outside.zoneAwareStack);
+      expect(inside.stack).toEqual(inside.zoneAwareStack);
+      expect(typeof inside.originalStack).toEqual('string');
+      const outsideFrames = outside.stack.split(/\n/);
+      const insideFrames = inside.stack.split(/\n/);
+      // throw away first line if it contains the error
+      if (/Outside/.test(outsideFrames[0])) {
+        outsideFrames.shift();
+      }
+      if (/new Error/.test(outsideFrames[0])) {
+        outsideFrames.shift();
+      }
+      if (/Inside/.test(insideFrames[0])) {
+        insideFrames.shift();
+      }
+      if (/new Error/.test(insideFrames[0])) {
+        insideFrames.shift();
+      }
+
+      expect(outsideFrames[0]).toMatch(/testFn.*[<root>]/);
+
+      expect(insideFrames[0]).toMatch(/insideRun.*[InnerZone]]/);
+      expect(insideFrames[1]).toMatch(/run.*[<root> => InnerZone]]/);
+      expect(insideFrames[2]).toMatch(/testFn.*[<root>]]/);
+    }
+  });
+});
+
+function getRootZone() {
+  let zone = Zone.current;
+  while(zone.parent) {
+    zone = zone.parent;
+  }
+  return zone;
+}
