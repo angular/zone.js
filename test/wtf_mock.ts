f79c1790f36b9e5d1f8f50220a8e2e0160f5d798
Misko Hevery <misko@hevery.com>
2016-11-17 16:02:46 -0800

    chore: explicitly load zone.js (instead from system.js)
    System.js loads zone.js using XHR and then evals. In Safari the stack 
    traces come with no line numbers as it does not honor inline 
    source-maps. This prevents stack analysis. By loading it explicitly 
    using script tag the zone.js has proper line numbers for its stack 
    traces.

--- test/wtf_mock.ts
+++ test/wtf_mock.ts
@@ -7,84 +7,87 @@
  */
 
 'use strict';
-
-var log = [];
-var logArgs = [];
-var wtfMock = {
-  log: log,
-  logArgs: logArgs,
-  reset: function() {
-    log.length = 0;
-    logArgs.length = 0;
-  },
-  trace: {
-    leaveScope: function(scope, returnValue) {
-      return scope(returnValue);
+(function(global) {
+  var log = [];
+  var logArgs = [];
+  var wtfMock = {
+    log: log,
+    logArgs: logArgs,
+    reset: function() {
+      log.length = 0;
+      logArgs.length = 0;
     },
-    beginTimeRange: function(type, action) {
-      logArgs.push([]);
-      log.push('>>> ' + type + '[' + action + ']');
-      return function() {
+    trace: {
+      leaveScope: function(scope, returnValue) {
+        return scope(returnValue);
+      },
+      beginTimeRange: function(type, action) {
         logArgs.push([]);
-        log.push('<<< ' + type);
-      };
-    },
-    endTimeRange: function(range) {
-      range();
-    },
-    events: {
-      createScope: function(signature, flags) {
-        var parts = signature.split('(');
-        var name = parts[0];
-        return function scopeFn() {
-          var args = [];
-          for (var i = arguments.length - 1; i >= 0; i--) {
-            var arg = arguments[i];
-            if (arg !== undefined) {
-              args.unshift(__stringify(arg));
-            }
-          }
-          log.push('> ' + name + '(' + args.join(', ') + ')');
-          logArgs.push(args);
-          return function(retValue) {
-            log.push('< ' + name + (retValue == undefined ? '' : ' => ' + retValue));
-            logArgs.push(retValue);
-            return retValue;
-          };
+        log.push('>>> ' + type + '[' + action + ']');
+        return function() {
+          logArgs.push([]);
+          log.push('<<< ' + type);
         };
       },
-      createInstance: function(signature, flags) {
-        var parts = signature.split('(');
-        var name = parts[0];
-        return function eventFn() {
-          var args = [];
-          for (var i = arguments.length - 1; i >= 0; i--) {
-            var arg = arguments[i];
-            if (arg !== undefined) {
-              args.unshift(__stringify(arg));
+      endTimeRange: function(range) {
+        range();
+      },
+      events: {
+        createScope: function(signature, flags) {
+          var parts = signature.split('(');
+          var name = parts[0];
+          return function scopeFn() {
+            var args = [];
+            for (var i = arguments.length - 1; i >= 0; i--) {
+              var arg = arguments[i];
+              if (arg !== undefined) {
+                args.unshift(__stringify(arg));
+              }
             }
-          }
-          log.push('# ' + name + '(' + args.join(', ') + ')');
-          logArgs.push(args);
-        };
+            log.push('> ' + name + '(' + args.join(', ') + ')');
+            logArgs.push(args);
+            return function(retValue) {
+              log.push('< ' + name + (retValue == undefined ? '' : ' => ' + retValue));
+              logArgs.push(retValue);
+              return retValue;
+            };
+          };
+        },
+        createInstance: function(signature, flags) {
+          var parts = signature.split('(');
+          var name = parts[0];
+          return function eventFn() {
+            var args = [];
+            for (var i = arguments.length - 1; i >= 0; i--) {
+              var arg = arguments[i];
+              if (arg !== undefined) {
+                args.unshift(__stringify(arg));
+              }
+            }
+            log.push('# ' + name + '(' + args.join(', ') + ')');
+            logArgs.push(args);
+          };
+        }
       }
     }
+  };
+
+  function __stringify(obj) {
+    var str = typeof obj == 'string' || !obj ? JSON.stringify(obj) : obj.toString();
+    if (str == '[object Arguments]') {
+      str = JSON.stringify(Array.prototype.slice.call(obj));
+    } else if (str == '[object Object]') {
+      str = JSON.stringify(obj);
+    }
+    return str;
   }
-};
 
-function __stringify(obj) {
-  var str = typeof obj == 'string' || !obj ? JSON.stringify(obj) : obj.toString();
-  if (str == '[object Arguments]') {
-    str = JSON.stringify(Array.prototype.slice.call(obj));
-  } else if (str == '[object Object]') {
-    str = JSON.stringify(obj);
-  }
-  return str;
-}
+  beforeEach(function() {
+    wtfMock.reset();
+  });
 
-beforeEach(function() {
-  wtfMock.reset();
-});
+  (<any>global).wtfMock = wtfMock;
+  (<any>global).wtf = wtfMock;
+})(typeof window === 'object' && window || typeof self === 'object' && self || global);
 
-(<any>global).wtfMock = wtfMock;
-(<any>global).wtf = wtfMock;
+declare var wtfMock: any;
