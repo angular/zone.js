6d31734dfc87781d629bd364028dbde0e63afff6
JiaLiPassion <JiaLi.Passion@gmail.com>
2017-01-24 08:33:05 +0900

    feat(electron/nw): fix #533, in electron/nw.js, we may need to patch both browser API and nodejs API, so we need a zone-mix.js to contains both patched API.

--- gulpfile.js
+++ gulpfile.js
@@ -82,6 +82,11 @@ gulp.task('build/zone.js', ['compile-esm'], function(cb) {
   return generateScript('./lib/browser/rollup-main.ts', 'zone.js', false, cb);
 });
 
+// Zone for electron/nw environment.
+gulp.task('build/zone-mix.js', ['compile-esm'], function(cb) {
+    return generateScript('./lib/mix/rollup-mix.ts', 'zone-mix.js', false, cb);
+});
+
 gulp.task('build/zone.min.js', ['compile-esm'], function(cb) {
   return generateScript('./lib/browser/rollup-main.ts', 'zone.min.js', true, cb);
 });
@@ -161,6 +166,7 @@ gulp.task('build', [
   'build/zone-node.js',
   'build/webapis-media-query.js',
   'build/webapis-notification.js',
+  'build/zone-mix.js',
   'build/jasmine-patch.js',
   'build/jasmine-patch.min.js',
   'build/mocha-patch.js',
