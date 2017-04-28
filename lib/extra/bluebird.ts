95f4126431e211cbbb875ea981a7cee829539696
JiaLi.Passion <JiaLi.Passion@gmail.com>
2017-04-29 01:04:04 +0900

    feat(patch): load non standard api with new load module method

--- lib/extra/bluebird.ts
+++ lib/extra/bluebird.ts
@@ -5,16 +5,15 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-(() => {
-  const __symbol__ = (Zone as any).__symbol__;
+Zone.__load_patch('bluebird', (global: any, Zone: ZoneType, api: _ZonePrivate) => {
   // TODO: @JiaLiPassion, we can automatically patch bluebird
   // if global.Promise = Bluebird, but sometimes in nodejs,
   // global.Promise is not Bluebird, and Bluebird is just be
   // used by other libraries such as sequelize, so I think it is
   // safe to just expose a method to patch Bluebird explicitly
-  (Zone as any)[__symbol__('bluebird')] = function patchBluebird(Bluebird: any) {
+  (Zone as any)[Zone.__symbol__('bluebird')] = function patchBluebird(Bluebird: any) {
     Bluebird.setScheduler((fn: Function) => {
       Zone.current.scheduleMicroTask('bluebird', fn);
     });
   };
-})();
+});
