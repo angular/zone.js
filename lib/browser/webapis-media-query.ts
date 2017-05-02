97c03b533d83c71f3ac00cc5fe9474833037f7f6
JiaLiPassion <JiaLi.Passion@gmail.com>
2017-05-03 03:09:02 +0900

    feat(patch): load non standard api with new load module method (#764)

--- lib/browser/webapis-media-query.ts
+++ lib/browser/webapis-media-query.ts
@@ -5,40 +5,35 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-((_global: any) => {
-  // patch MediaQuery
-  patchMediaQuery(_global);
-
-  function patchMediaQuery(_global: any) {
-    if (!_global['MediaQueryList']) {
-      return;
-    }
-    const patchEventTargetMethods =
-        (Zone as any)[(Zone as any).__symbol__('patchEventTargetMethods')];
-    patchEventTargetMethods(
-        _global['MediaQueryList'].prototype, 'addListener', 'removeListener',
-        (self: any, args: any[]) => {
-          return {
-            useCapturing: false,
-            eventName: 'mediaQuery',
-            handler: args[0],
-            target: self || _global,
-            name: 'mediaQuery',
-            invokeAddFunc: function(addFnSymbol: any, delegate: any) {
-              if (delegate && (<Task>delegate).invoke) {
-                return this.target[addFnSymbol]((<Task>delegate).invoke);
-              } else {
-                return this.target[addFnSymbol](delegate);
-              }
-            },
-            invokeRemoveFunc: function(removeFnSymbol: any, delegate: any) {
-              if (delegate && (<Task>delegate).invoke) {
-                return this.target[removeFnSymbol]((<Task>delegate).invoke);
-              } else {
-                return this.target[removeFnSymbol](delegate);
-              }
-            }
-          };
-        });
+Zone.__load_patch('mediaQuery', (global: any, Zone: ZoneType, api: _ZonePrivate) => {
+  if (!global['MediaQueryList']) {
+    return;
   }
-})(typeof window === 'object' && window || typeof self === 'object' && self || global);
+  const patchEventTargetMethods =
+      (Zone as any)[(Zone as any).__symbol__('patchEventTargetMethods')];
+  patchEventTargetMethods(
+      _global['MediaQueryList'].prototype, 'addListener', 'removeListener',
+      (self: any, args: any[]) => {
+        return {
+          useCapturing: false,
+          eventName: 'mediaQuery',
+          handler: args[0],
+          target: self || global,
+          name: 'mediaQuery',
+          invokeAddFunc: function(addFnSymbol: any, delegate: any) {
+            if (delegate && (<Task>delegate).invoke) {
+              return this.target[addFnSymbol]((<Task>delegate).invoke);
+            } else {
+              return this.target[addFnSymbol](delegate);
+            }
+          },
+          invokeRemoveFunc: function(removeFnSymbol: any, delegate: any) {
+            if (delegate && (<Task>delegate).invoke) {
+              return this.target[removeFnSymbol]((<Task>delegate).invoke);
+            } else {
+              return this.target[removeFnSymbol](delegate);
+            }
+          }
+        };
+      });
+});
--- lib/browser/webapis-notification.ts
+++ lib/browser/webapis-notification.ts
@@ -5,20 +5,15 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-((_global: any) => {
-  // patch Notification
-  patchNotification(_global);
-
-  function patchNotification(_global: any) {
-    const Notification = _global['Notification'];
-    if (!Notification || !Notification.prototype) {
-      return;
-    }
-    const desc = Object.getOwnPropertyDescriptor(Notification.prototype, 'onerror');
-    if (!desc || !desc.configurable) {
-      return;
-    }
-    const patchOnProperties = (Zone as any)[(Zone as any).__symbol__('patchOnProperties')];
-    patchOnProperties(Notification.prototype, null);
+Zone.__load_patch('notification', (global: any, Zone: ZoneType, api: _ZonePrivate) => {
+  const Notification = _global['Notification'];
+  if (!Notification || !Notification.prototype) {
+    return;
   }
-})(typeof window === 'object' && window || typeof self === 'object' && self || global);
+  const desc = Object.getOwnPropertyDescriptor(Notification.prototype, 'onerror');
+  if (!desc || !desc.configurable) {
+    return;
+  }
+  const patchOnProperties = (Zone as any)[(Zone as any).__symbol__('patchOnProperties')];
+  patchOnProperties(Notification.prototype, null);
+});
