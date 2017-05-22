9dc4fa18775902e6510e57dc5d69ee6c4e484328
JiaLi.Passion <JiaLi.Passion@gmail.com>
2017-05-22 22:39:01 +0900

    fix(patch): fix #791, fix mediaQuery/Notification patch use wrong global

--- lib/browser/webapis-notification.ts
+++ lib/browser/webapis-notification.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 Zone.__load_patch('notification', (global: any, Zone: ZoneType, api: _ZonePrivate) => {
-  const Notification = _global['Notification'];
+  const Notification = global['Notification'];
   if (!Notification || !Notification.prototype) {
     return;
   }
