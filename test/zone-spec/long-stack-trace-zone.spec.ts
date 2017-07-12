273cb8591e637ec0b396bdee31a8b54462a436a0
JiaLiPassion <JiaLi.Passion@gmail.com>
2017-07-13 05:57:03 +0900

    fix(websocket): fix #824, patch websocket onproperties correctly in PhantomJS (#826)
    * fix(websocket): fix #824, patch websocket onproperties correctly in phantomjs
    
    * add phantomjs test for local and travis

--- test/zone-spec/long-stack-trace-zone.spec.ts
+++ test/zone-spec/long-stack-trace-zone.spec.ts
@@ -7,136 +7,139 @@
  */
 
 import {zoneSymbol} from '../../lib/common/utils';
+import {ifEnvSupports, isSupportSetErrorStack} from '../test-util';
+
 const defineProperty = (Object as any)[zoneSymbol('defineProperty')] || Object.defineProperty;
 
-describe('longStackTraceZone', function() {
-  let log: Error[];
-  let lstz: Zone;
-  let longStackTraceZoneSpec = (Zone as any)['longStackTraceZoneSpec'];
+describe(
+    'longStackTraceZone', ifEnvSupports(isSupportSetErrorStack, function() {
+      let log: Error[];
+      let lstz: Zone;
+      let longStackTraceZoneSpec = (Zone as any)['longStackTraceZoneSpec'];
 
-  beforeEach(function() {
-    lstz = Zone.current.fork(longStackTraceZoneSpec).fork({
-      name: 'long-stack-trace-zone-test',
-      onHandleError: (parentZoneDelegate: ZoneDelegate, currentZone: Zone, targetZone: Zone,
-                      error: any): boolean => {
-        parentZoneDelegate.handleError(targetZone, error);
-        log.push(error);
-        return false;
-      }
-    });
+      beforeEach(function() {
+        lstz = Zone.current.fork(longStackTraceZoneSpec).fork({
+          name: 'long-stack-trace-zone-test',
+          onHandleError: (parentZoneDelegate: ZoneDelegate, currentZone: Zone, targetZone: Zone,
+                          error: any): boolean => {
+            parentZoneDelegate.handleError(targetZone, error);
+            log.push(error);
+            return false;
+          }
+        });
 
-    log = [];
-  });
-
-  function expectElapsed(stack: string, expectedCount: number) {
-    try {
-      let actualCount = stack.split('_Elapsed_').length;
-      if (actualCount !== expectedCount) {
-        expect(actualCount).toEqual(expectedCount);
-        console.log(stack);
-      }
-    } catch (e) {
-      expect(e).toBe(null);
-    }
-  }
-
-  it('should produce long stack traces', function(done) {
-    lstz.run(function() {
-      setTimeout(function() {
-        setTimeout(function() {
-          setTimeout(function() {
-            expectElapsed(log[0].stack, 3);
-            done();
-          }, 0);
-          throw new Error('Hello');
-        }, 0);
-      }, 0);
-    });
-  });
-
-  it('should produce a long stack trace even if stack setter throws', (done) => {
-    let wasStackAssigned = false;
-    let error = new Error('Expected error');
-    defineProperty(error, 'stack', {
-      configurable: false,
-      get: () => 'someStackTrace',
-      set: (v: any) => {
-        throw new Error('no writes');
-      }
-    });
-    lstz.run(() => {
-      setTimeout(() => {
-        throw error;
+        log = [];
       });
-    });
-    setTimeout(() => {
-      const e = log[0];
-      expect((e as any).longStack).toBeTruthy();
-      done();
-    });
-  });
 
-  it('should produce long stack traces when has uncaught error in promise', function(done) {
-    lstz.runGuarded(function() {
-      setTimeout(function() {
-        setTimeout(function() {
-          let promise = new Promise(function(resolve, reject) {
-            setTimeout(function() {
-              reject(new Error('Hello Promise'));
-            }, 0);
-          });
-          promise.then(function() {
-            fail('should not get here');
-          });
+      function expectElapsed(stack: string, expectedCount: number) {
+        try {
+          let actualCount = stack.split('_Elapsed_').length;
+          if (actualCount !== expectedCount) {
+            expect(actualCount).toEqual(expectedCount);
+            console.log(stack);
+          }
+        } catch (e) {
+          expect(e).toBe(null);
+        }
+      }
+
+      it('should produce long stack traces', function(done) {
+        lstz.run(function() {
           setTimeout(function() {
-            expectElapsed(log[0].stack, 5);
-            done();
-          }, 0);
-        }, 0);
-      }, 0);
-    });
-  });
-
-  it('should produce long stack traces when handling error in promise', function(done) {
-    lstz.runGuarded(function() {
-      setTimeout(function() {
-        setTimeout(function() {
-          let promise = new Promise(function(resolve, reject) {
             setTimeout(function() {
-              try {
-                throw new Error('Hello Promise');
-              } catch (err) {
-                reject(err);
-              }
+              setTimeout(function() {
+                expectElapsed(log[0].stack, 3);
+                done();
+              }, 0);
+              throw new Error('Hello');
             }, 0);
-          });
-          promise.catch(function(error) {
-            // should be able to get long stack trace
-            const longStackFrames: string = longStackTraceZoneSpec.getLongStackTrace(error);
-            expectElapsed(longStackFrames, 4);
-            done();
-          });
-        }, 0);
-      }, 0);
-    });
-  });
-
-  it('should not produce long stack traces if Error.stackTraceLimit = 0', function(done) {
-    const originalStackTraceLimit = Error.stackTraceLimit;
-    lstz.run(function() {
-      setTimeout(function() {
-        setTimeout(function() {
-          setTimeout(function() {
-            if (log[0].stack) {
-              expectElapsed(log[0].stack, 1);
-            }
-            Error.stackTraceLimit = originalStackTraceLimit;
-            done();
           }, 0);
-          Error.stackTraceLimit = 0;
-          throw new Error('Hello');
-        }, 0);
-      }, 0);
-    });
-  });
-});
+        });
+      });
+
+      it('should produce a long stack trace even if stack setter throws', (done) => {
+        let wasStackAssigned = false;
+        let error = new Error('Expected error');
+        defineProperty(error, 'stack', {
+          configurable: false,
+          get: () => 'someStackTrace',
+          set: (v: any) => {
+            throw new Error('no writes');
+          }
+        });
+        lstz.run(() => {
+          setTimeout(() => {
+            throw error;
+          });
+        });
+        setTimeout(() => {
+          const e = log[0];
+          expect((e as any).longStack).toBeTruthy();
+          done();
+        });
+      });
+
+      it('should produce long stack traces when has uncaught error in promise', function(done) {
+        lstz.runGuarded(function() {
+          setTimeout(function() {
+            setTimeout(function() {
+              let promise = new Promise(function(resolve, reject) {
+                setTimeout(function() {
+                  reject(new Error('Hello Promise'));
+                }, 0);
+              });
+              promise.then(function() {
+                fail('should not get here');
+              });
+              setTimeout(function() {
+                expectElapsed(log[0].stack, 5);
+                done();
+              }, 0);
+            }, 0);
+          }, 0);
+        });
+      });
+
+      it('should produce long stack traces when handling error in promise', function(done) {
+        lstz.runGuarded(function() {
+          setTimeout(function() {
+            setTimeout(function() {
+              let promise = new Promise(function(resolve, reject) {
+                setTimeout(function() {
+                  try {
+                    throw new Error('Hello Promise');
+                  } catch (err) {
+                    reject(err);
+                  }
+                }, 0);
+              });
+              promise.catch(function(error) {
+                // should be able to get long stack trace
+                const longStackFrames: string = longStackTraceZoneSpec.getLongStackTrace(error);
+                expectElapsed(longStackFrames, 4);
+                done();
+              });
+            }, 0);
+          }, 0);
+        });
+      });
+
+      it('should not produce long stack traces if Error.stackTraceLimit = 0', function(done) {
+        const originalStackTraceLimit = Error.stackTraceLimit;
+        lstz.run(function() {
+          setTimeout(function() {
+            setTimeout(function() {
+              setTimeout(function() {
+                if (log[0].stack) {
+                  expectElapsed(log[0].stack, 1);
+                }
+                Error.stackTraceLimit = originalStackTraceLimit;
+                done();
+              }, 0);
+              Error.stackTraceLimit = 0;
+              throw new Error('Hello');
+            }, 0);
+          }, 0);
+        });
+      });
+    }));
