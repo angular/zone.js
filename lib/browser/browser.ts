df625e113b285495bd23183c16281bce2af5a814
Vikram Subramanian <vikram.subramanian@gmail.com>
2017-07-31 22:48:50 -0700

    fix: suppress closure warnings/errors

--- lib/browser/browser.ts
+++ lib/browser/browser.ts
@@ -5,6 +5,10 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+/**
+ * @fileoverview
+ * @suppress {missingRequire}
+ */
 
 import {findEventTasks} from '../common/events';
 import {patchTimer} from '../common/timers';
