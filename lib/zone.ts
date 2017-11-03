9cf4db9b149ba23af36c222cf4580ebe0ccfc7bf
Evan Martin <martine@danga.com>
2017-11-03 15:56:37 -0700

    add missing star in zone jsdoc
    Without this extra star, the Closure commands in the jsdoc are ignored.

--- lib/zone.ts
+++ lib/zone.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-/*
+/**
  * Suppress closure compiler errors about unknown 'global' variable
  * @fileoverview
  * @suppress {undefinedVars}
