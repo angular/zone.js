7afb895ac27e04cf77b671870b6a25cb8fd6cf1d
JiaLi.Passion <JiaLi.Passion@gmail.com>
2018-01-04 01:08:23 +0900

    feat(asynchooks): add examples

--- lib/node/rollup-main-asynchooks.ts
+++ lib/node/rollup-main-asynchooks.ts
@@ -5,8 +5,14 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-
 import '../zone';
 import '../common/promise';
 import '../common/to-string';
+const _global = global as any;
+_global.__Zone_disable_node_timers = true;
+_global.__Zone_disable_nextTick = true;
+_global.__Zone_disable_handleUnhandledPromiseRejection = true;
+_global.__Zone_disable_crypto = true;
+_global.__Zone_disable_fs = true;
+import './node';
 import './async_hooks';
