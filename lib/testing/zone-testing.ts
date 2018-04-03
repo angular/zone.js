744594816d907e32cc55ee163652131f32a5a32d
JiaLi.Passion <JiaLi.Passion@gmail.com>
2018-04-03 11:43:55 +0900

    fix(test): add async/fakeAsync into zone-testing bundle

--- lib/testing/zone-testing.ts
+++ lib/testing/zone-testing.ts
@@ -11,6 +11,6 @@ import '../zone-spec/long-stack-trace';
 import '../zone-spec/proxy';
 import '../zone-spec/sync-test';
 import '../jasmine/jasmine';
-import '../zone-spec/async-test';
-import '../zone-spec/fake-async-test';
+import './async-testing';
+import './fake-async';
 import './promise-testing';
