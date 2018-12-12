afa1363eb9ae3be506442311a18723a112f9ef8b
JiaLiPassion <JiaLi.Passion@gmail.com>
2018-12-13 06:36:43 +0900

    fix(core): fix #1153, ZoneTask.toString should always be a string (#1166)

--- test/common/toString.spec.ts
+++ test/common/toString.spec.ts
@@ -47,3 +47,43 @@ describe('global function patch', () => {
        }));
   });
 });
+
+describe('ZoneTask', () => {
+  it('should return handleId.toString if handleId is available', () => {
+    let macroTask1: any = undefined;
+    let macroTask2: any = undefined;
+    let microTask: any = undefined;
+    const zone = Zone.current.fork({
+      name: 'timer',
+      onScheduleTask: (delegate: ZoneDelegate, curr: Zone, target: Zone, task: Task) => {
+        if (task.type === 'macroTask') {
+          if (!macroTask1) {
+            macroTask1 = task;
+          } else {
+            macroTask2 = task;
+          }
+        } else if (task.type === 'microTask') {
+          microTask = task;
+        }
+        return task;
+      }
+    });
+    zone.run(() => {
+      const id1 = setTimeout(() => {});
+      clearTimeout(id1);
+      const id2 = setTimeout(() => {});
+      clearTimeout(id2);
+      Promise.resolve().then(() => {});
+      const macroTask1Str = macroTask1.toString();
+      const macroTask2Str = macroTask2.toString();
+      expect(typeof macroTask1Str).toEqual('string');
+      expect(macroTask1Str).toEqual(id1.toString());
+      expect(typeof macroTask2Str).toEqual('string');
+      expect(macroTask2Str).toEqual(id2.toString());
+      if (macroTask1.data && typeof macroTask1.data.handleId === 'number') {
+        expect(macroTask1Str).not.toEqual(macroTask2Str);
+      }
+      expect(typeof microTask.toString()).toEqual('string');
+    });
+  });
+});
